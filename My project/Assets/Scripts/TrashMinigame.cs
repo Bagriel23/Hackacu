ff616ffd1e5154e3e56788f5f97b48daad027604
Gabriel Dantas <gabrielsoudantas@gmail.com>
2024-09-29 20:52:30 -0400

    "Pronto" para build

--- My project/Assets/Scripts/TrashMinigame.cs
+++ My project/Assets/Scripts/TrashMinigame.cs
@@ -2,39 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TrashMinigame : MonoBehaviour
 {
-    //[SerializeField] private GameObject trashCan;
+    [SerializeField] private GameObject trashCan;
     [SerializeField] private GameObject trashCanCanvas;
+    [SerializeField] private GameObject trash;
     [SerializeField] private Text CanCountCanvas;
-    private static int trashCanCount;
+    public static int trashCanCount;
     [SerializeField] LayerMask playerLayer;
     [SerializeField] private float radius;
     private bool onRadius;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         trashCanCount = 0;
         UpdateCanCount();
     }
-
-    // Update is called once per frame
 
     void FixedUpdate()
     {
         Interact();
     }
+
     void Update()
     {
         if (onRadius && Input.GetKeyDown(KeyCode.E))
         {
             PickUp();
         }
+        if (trashCanCount == 8)
+        {
+            SceneManager.LoadScene("Praia");
+        }
     }
 
-    public void  Interact()
+    public void Interact()
     {
         Collider2D hit = Physics2D.OverlapCircle(transform.position, radius, playerLayer);
         if (hit != null)
@@ -46,22 +52,23 @@ public class TrashMinigame : MonoBehaviour
             onRadius = false;
         }
     }
+
     private void PickUp()
     {
-            if (trashCanCount == 0)
-            {
-                trashCanCanvas.SetActive(true);
-            }
+        if (trashCanCount == 0)
+        {
+            trashCanCanvas.SetActive(true);
+        }
         trashCanCount ++;
         UpdateCanCount();
-        gameObject.SetActive(false);
-        Debug.Log(trashCanCount);
+        trashCan.SetActive(false);
     }
 
     private void UpdateCanCount()
     {
         CanCountCanvas.text = " x" + trashCanCount;
     }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position, radius);
